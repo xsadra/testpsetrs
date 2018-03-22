116ae125c6cdb3115a2aea93b5277c4940f30b35
Sadra <ti9mail@gmail.com>
2018-03-22 07:47:28 +0100

    Create new Spring project to test psets

---
+++ src/main/java/de/sadrab/testpsets/TestpsetsApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.testpsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpsetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpsetsApplication.class, args);
+	}
+}
